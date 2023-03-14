5ca3ba1143e27ecd3d4cd6f16dd5c9b394236faa
Minuscu1e <892422848@qq.com>
2023-03-14 14:35:48 +0800

    添加

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 </script>
 
 <template>
+  <div id="app">
     <router-view></router-view>
+  </div>
 </template>
 
-<style scoped>
+<style>
 
 </style>
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,16 +1,66 @@
 <template>
-  <div>{{ count }}</div>
-  <el-button @click="count++">+</el-button>
-  <el-button @click="count--">-</el-button>
-  <el-button @click="change">change</el-button>
+  <div class="diagram" id="diagram" name="diagram"></div>
+  <div class="overview" id="overview" name="overview"></div>
 </template>
 
 <script lang="ts" setup>
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
+import go, {DiagramInitOptions} from 'gojs'
 
-let count: number = ref(0)
+onMounted(() => {
+  init();
+})
 
-function change() {
-  count.value = 10
+
+function init() {
+  let diagram = new go.Diagram('diagram',
+    {
+      "initialContentAlignment": go.Spot.Center,
+      // 设置为无限滚动
+      "scrollMode": go.Diagram.InfiniteScroll,
+      "allowZoom": true,
+      "grid.visible": true,
+      "allowDelete": false,
+      "undoManager.isEnabled": true
+    })
+  // 创建预览地图
+  let overview = new go.Overview("overview", {
+    observed: diagram, contentAlignment: go.Spot.Center
+  })
+  overview.box.elt(0).stroke = "green";
+
+  // 创建一个节点
+  let node = new go.Node(go.Panel.Auto)
+  let shape = new go.Shape({
+    figure: "RoundedRectangle",
+    fill: "lightblue"
+  })
+  node.add(shape)
+  var text = new go.TextBlock(
+    {text: "Hello", margin: 5}
+  )
+  node.add(text)
+
+  diagram.add(node)
 }
+
 </script>
+
+<style scoped>
+.diagram {
+  width: 100%;
+  height: 100vh;
+  background: aliceblue;
+}
+
+.overview {
+  width: 200px;
+  height: 200px;
+
+  position: absolute;
+  z-index: 999;
+  top: 20px;
+  right: 20px;
+
+}
+</style>
